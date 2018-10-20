89d0d431b342bfac01206d16089aecf52f3e02fc
avhirup <avhirup84jazz@gmail.com>
2018-10-20 11:18:48 +0530

    version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='cable',
-      version='0.0.1',
+      version='0.0.2',
       description='This package has components  for preprocessing',
       author='Avhirup',
       author_email='avhirupchakraborty@gmail.com',
