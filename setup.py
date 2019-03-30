833a06ef61ef12378de492c05d1047d96ff2f8be
avhirup <avhirup84jazz@gmail.com>
2019-03-30 11:31:39 +0530

    cable_cmd

--- setup.py
+++ setup.py
@@ -2,6 +2,18 @@
 
 from setuptools import setup, find_packages
 import os
+import subprocess
+#adding path to bash profile
+installation_path=os.__file__[:-5]+'site-packages/cable/cd '
+home_path= subprocess.Popen("echo $HOME", shell=True, stdout=subprocess.PIPE).stdout.read().strip().decode('utf-8')
+
+with open(home_path+"/.bash_profile",'a') as f:
+	f.write("added by cable installer\n")
+	f.write('export PATH=$PATH":'+installation_path+"\n")
+
+with open(home_path+"/.bashrc",'a') as f:
+	f.write("added by cable installer\n")
+	f.write('export PATH=$PATH":'+installation_path+"\n")
 
 setup(name='cable',
       version='0.0.3',
